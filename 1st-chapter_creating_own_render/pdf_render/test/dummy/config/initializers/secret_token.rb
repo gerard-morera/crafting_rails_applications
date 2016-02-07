57fa2c2d2d167653ce7fff938be53ebcef7b674f
gerard-morera <grdmorera@icloud.com>
2016-02-07 20:03:05 +0100

    scaffolding a rails pluggin

---
+++ 1st-chapter_creating_own_render/pdf_render/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '40dab73ad8e74afa043568983ed6b113a0fd6901e29c450a36012e1f2d631333169c7f8937308977d3d9ad858c2e2de54bdfacd882a1ea925ed7bee7ac3331fb'
